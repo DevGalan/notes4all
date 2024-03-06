f912b661125ead505637d7fae10f639125e0a11c
José Manuel Galán <devgalan@gmail.com>
2024-03-06 15:43:30 +0100

    Finished note-form

--- resources/views/notes/note-form.blade.php
+++ resources/views/notes/note-form.blade.php
@@ -14,18 +14,6 @@
                 <input type="text" name="name" value="{{ $note->name }}">
                 <input type="text" name="text" value="{{ $note->text }}">
                 <input type="text" name="category" value="{{ $note->category }}">
-                <button type="submit">Subir</button>
-            </form>
-
-            <form th:action="@{/notes}" method="post" th:object="${note}">
-                <div class="mb-3">
-                    <label for="title" class="form-label">Título</label>
-                    <input type="text" class="form-control" id="title" th:field="*{title}" required>
-                </div>
-                <div class="mb-3">
-                    <label for="content" class="form-label">Contenido</label>
-                    <textarea type="text" class="form-control" id="content" th:field="*{content}" th:text="${note.content}" required></textarea>
-                </div>
                 <button type="submit" class="btn btn-success btn-circle btn-xl bnt-bottom-right d-flex flex-column justify-content-center align-items-center"><i class="bi-check2 text-white fs-2"></i></button>
             </form>
         </div>
